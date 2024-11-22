17fee7471e3aebac5cf2a616885270c7814d6409
deborah-ajalla <deborahajalla@gmail.com>
2024-11-22 16:33:16 -0300

    menu

--- main.py
+++ main.py
@@ -11,3 +11,28 @@
 """
 #-----------------------------------------------------------------
 print ("--------------------------------------------------------------")
+
+#----> MENU: <----
+while True:
+        print("*\n \t --- Sistema de Gestion de Pacientes de Centro de Estética ---")
+        print("\t 1- Ingresar Nuevo Paciente")
+        print("\t 2- Modificar datos de Paciente ")
+        print("\t 3- Mostrar datos de Paciente")
+        print("\t 4- Eliminar datos de Paciente")
+        print("\t 5- Salir")
+
+        opcion= input("Seleccione una opcion: ")
+        if opcion =="1":
+           print ("nuevo paciente")
+        elif opcion =="2":
+            print ("modificar datos de paciente")
+        elif opcion =="3":
+            print ("Mostrar datos de paciente")
+        elif opcion =="4":
+            print ("eliminar datos de paciente")
+        elif opcion =="5":
+            print("Hasta la próxima!")
+            break
+        else :
+            print("Opción Inválida")
+          
