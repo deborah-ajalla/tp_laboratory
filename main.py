0ac7cbf8947c160ab791406a32b549cd1c4cebda
deborah-ajalla <deborahajalla@gmail.com>
2024-11-22 23:28:30 -0300

    desarrollo de menu opcion 1,2 y 4

--- main.py
+++ main.py
@@ -20,38 +20,50 @@ c.conectar()
 
 # --> pruebo carga datos <--
 persona = {
-    "dni": "26896328",
+    "dni": "26896332",
     "nombre": "Pablo",
-    "edad": 30,
+    "edad": "abcd",
     "apellido": "Torrez",
     "genero":  "masculino",
-    "celular": "1562358967",
-    "mail": "algo2@gmail.com"
+    "celular": "1562358970",
+    "mail": "algo6@gmail.com"
 }
 prueba = p.carga_datos(persona)
 print("prueba de carga de paciente")
 print(prueba)
 
+# --> pruebo listar <--
+# listado = p.mostrar_pacientes()
+# print("prueba de listado de paciente")
+# print(listado)
+
+buscar = p.buscar_paciente("26896338")
+print("prueba de buscar un paciente")
+print(buscar)
 
 #----> MENU: <----
 while True:
         print("*\n \t --- Sistema de Gestion de Pacientes de Centro de Estética ---")
         print("\t 1- Ingresar Nuevo Paciente")
-        print("\t 2- Modificar datos de Paciente ")
-        print("\t 3- Mostrar datos de Paciente")
-        print("\t 4- Eliminar datos de Paciente")
-        print("\t 5- Salir")
+        print("\t 2- Buscar Paciente")
+        print("\t 3- Modificar datos")
+        print("\t 4- Mostrar Listado Total")  
+        print("\t 5- Eliminar datos de Paciente")
+        print("\t 6- Salir")
 
         opcion= input("Seleccione una opcion: ")
         if opcion =="1":
            print ("nuevo paciente")
         elif opcion =="2":
-            print ("modificar datos de paciente")
+
+            print (p.buscar_paciente("26896332"))  # --> hacer que se ingrese el dni por teclado!!
         elif opcion =="3":
-            print ("Mostrar datos de paciente")
+            print ("Modificar datos")
         elif opcion =="4":
-            print ("eliminar datos de paciente")
+            print (p.mostrar_pacientes()) 
         elif opcion =="5":
+            print ("Eliminar datos de paciente")
+        elif opcion =="6":
             print("Hasta la próxima!")
             break
         else :
--- paciente_datos.py
+++ paciente_datos.py
@@ -1,6 +1,7 @@
 import conexion as c
 #-----------------------------------------------------------------
-
+# MENÚ OPCIÓN 1: INGRESAR NUEVO PACIENTE
+#-----------------------------------------------------------------
 def carga_datos (paciente):
     paciente = dict (paciente)
 
@@ -8,7 +9,7 @@ def carga_datos (paciente):
         db = c.conectar()
         cursor = db.cursor()
         columnas = tuple(paciente.keys())     # -> cargo en el campo seleccionado
-        valores = tuple(paciente.values())  # -> cargo registros
+        valores = tuple(paciente.values())    # -> cargo registros
 
         sql = """
               INSERT INTO pacientes {campos} VALUES (?,?,?,?,?,?,?)
@@ -20,16 +21,84 @@ def carga_datos (paciente):
         db.commit()
         
         if creado:
-            return {"respuesta": creado, "mensaje": "Paciente Registrado"}
-        
+            cursor.close()
+            db.close()
+            return {"respuesta": creado, "mensaje": "Paciente Registrado"} 
         else:
+            cursor.close()
+            db.close()
             return {"respuesta": creado, "mensaje": "No se ha podido realizar la acción"}
         
     except Exception as e:
+        if "UNIQUE" in  str (e) and "DNI" in str (e):                 # --> verifica que sólo haya un paciente por DNI
+            mensaje = "Ya existe un Paciente registrada con ese DNI"
+        elif "UNIQUE" in  str (e) and "MAIL" in str (e):              # --> verifica que sólo haya un MAIL por paciente
+            mensaje = "Ya existe un paciente que registró ese Mail, por favor indique otro..."
+        elif "UNIQUE" in  str (e) and "CELULAR" in str (e):           # --> verifica que sólo haya un CELULAR por paciente
+            mensaje = "Ya existe un paciente que registró ese Celular de Contacto, por favor indique otro..."
+        else:
+            mensaje = str(e) 
+            cursor.close()
+            db.close()
+             
         return {"respuesta": False,
-                "mensaje": str (e)}
+                "mensaje": mensaje}
     
-    finally:
+#-----------------------------------------------------------------
+# MENÚ OPCIÓN 2: BUSCAR PACIENTE (POR DNI)
+#-----------------------------------------------------------------
+def buscar_paciente (dni_buscado):
+    try:
+        db = c.conectar()
+        cursor = db.cursor()
+        cursor.execute ("SELECT * FROM pacientes WHERE DNI = '{dni}'".format(dni = dni_buscado))
+        resultado = cursor.fetchall()
+
+        if resultado:
+            info = resultado [0]
+            persona = {"id": info[0], 
+                       "nombre": info[1],
+                       "apellido": info[2], 
+                       "dni": info[3],
+                       "género": info [4],
+                       "edad": info [5],
+                       "celular": info [6],
+                       "mail": info [7]
+                       }
+            cursor.close()
+            db.close()
+            return {"respuesta": True, "persona": persona, "mensaje": "Paciente Encontrado!! ✔"}
+        else:
+            cursor.close()
+            db.close()
+            return {"respuesta":False, "mensaje": "❌ No hay Paciente Registrado con ese DNI ❌"}
+        
+    except Exception as e:
         cursor.close()
         db.close()
+        return {"respuesta": False, "mensaje": str (e)}
 
+
+#-----------------------------------------------------------------
+# MENÚ OPCIÓN 4: MOSTRAR LISTADO TOTAL
+#-----------------------------------------------------------------
+def mostrar_pacientes ():
+    try:
+        db = c.conectar()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM pacientes")
+        pacientes = cursor.fetchall()
+
+        if pacientes:
+            cursor.close()
+            db.close()
+            return {"respuesta": True, "pacientes": pacientes, "mensaje": "Listado todo OK ✔"}
+        else:
+            cursor.close()
+            db.close()
+            return {"respuesta": False ,"pacientes": pacientes,  "mensaje": "No hay Pacientes registrados aun... ❌"}
+    except Exception as e:
+        cursor.close()
+        db.close()
+        return {"respuesta": False, "mensaje": str (e)}
+    
